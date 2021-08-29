9fedf0ec7ba2e2eed30e01874d697686b93292ab
Ayush Chaudhary <acayush2002@gmail.com>
2021-08-29 15:42:03 +0530

    2d array search

--- Matrix_questions.cpp
+++ Matrix_questions.cpp
@@ -31,47 +31,92 @@
 
 //Matrix multiplication
 
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main() {
+// int num1,num2,num3;
+// cin>>num1>>num2>>num3;
+// int a[num1][num2];
+// int b[num2][num3];
+// int c[num1][num3];
+
+// for(int i=0;i<num1;i++){
+//     for(int j=0;j<num2;j++){
+//         cin>>a[i][j];
+//     }
+// }
+
+// for(int i=0;i<num2;i++){
+//     for(int j=0;j<num3;j++){
+//         cin>>b[i][j];
+//     }
+// }
+
+// for(int i=0;i<num2;i++){
+//     for(int j=0;j<num3;j++){
+//         c[i][j]=0;
+//     }
+// }
+
+// for(int i=0;i<num1;i++){
+//     for(int j=0;j<num3;j++){
+//         for(int k=0;k<num2;k++){
+//             c[i][j]+=a[i][k]*b[k][j];
+//         }
+//     }
+// }
+
+
+// for(int i=0;i<num1;i++){
+//     for(int j=0;j<num3;j++){
+//         cout<<c[i][j]<<" ";
+//     }
+//     cout<<endl;
+// }
+// }
+
+
+// 2D Array search
+//brute force- O(M*N)
+//Optimised- O(M+N)
 
 #include <bits/stdc++.h>
 using namespace std;
 int main() {
-int num1,num2,num3;
-cin>>num1>>num2>>num3;
-int a[num1][num2];
-int b[num2][num3];
-int c[num1][num3];
+int n,m;
+cin>>n>>m;
+int a[n][m];
 
-for(int i=0;i<num1;i++){
-    for(int j=0;j<num2;j++){
+
+for(int i=0;i<n;i++){
+    for(int j=0;j<m;j++){
         cin>>a[i][j];
     }
 }
 
-for(int i=0;i<num2;i++){
-    for(int j=0;j<num3;j++){
-        cin>>b[i][j];
+int k;
+cin>>k;
+
+int i,j;
+i=0;
+j=m-1;
+
+bool found=false;
+while(i<=n-1 && j>=0){
+    if(a[i][j]==k){
+        found=true;
+        break;
+    } else if(a[i][j]>k){
+        j--;
+    }else{
+        i++;
     }
 }
 
-for(int i=0;i<num2;i++){
-    for(int j=0;j<num3;j++){
-        c[i][j]=0;
-    }
+if(found){
+    cout<<"element found at "<<i<<" "<<j;
+}else{
+    cout<<"not found";
 }
 
-for(int i=0;i<num1;i++){
-    for(int j=0;j<num3;j++){
-        for(int k=0;k<num2;k++){
-            c[i][j]+=a[i][k]*b[k][j];
-        }
-    }
-}
-
-
-for(int i=0;i<num1;i++){
-    for(int j=0;j<num3;j++){
-        cout<<c[i][j]<<" ";
-    }
-    cout<<endl;
-}
 }
